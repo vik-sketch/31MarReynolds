bee638ca24319452aad0ab6b200985ac9e7b043a
vinay <vinay@DESKTOP-6HN77SJ>
2020-03-31 20:11:55 +0530

    second line of code

--- Dodda31March/src/pack1/Class1.java
+++ Dodda31March/src/pack1/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("001 first git app class");
+System.out.println("002 seconf  git app class");
+
 	}
 
 }
